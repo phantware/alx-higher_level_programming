80b55ae548c02be1122b154b4e8b59f80653bb54
phantware <phantmoney2011@gmail.com>
2022-05-31 20:12:20 +0100

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-# for letter in range(97, 123):
-#     if chr(letter) != 'q' and chr(letter) != 'e':
-#         print("{}".format(chr(letter)), end="")
-
-import string
-print("Alphabet from a-z:")
-for letter in string.ascii_lowercase:
-    if letter != 'q' and letter !='e':
-        print(letter, end =" ")
+""" Print letter from a - z excluding q and e"""
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
