467e4158dab60f9ac7116a61bc94c5a045ce3538
phantware <phantmoney2011@gmail.com>
2022-09-12 15:24:36 +0100

    103-object_fct.js understood

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12
-};
-console.log(myObject);
+  value: 12,
+}
+console.log(myObject)
 myObject.incr = function () {
-  this.value++;
-};
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
+  this.value++
+}
+myObject.incr()
+console.log(myObject)
+myObject.incr()
+console.log(myObject)
+myObject.incr()
+console.log(myObject)
