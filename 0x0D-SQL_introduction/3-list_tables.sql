df9d5adf3f81f11fd941ed4884c8d56953d261a5
phantware <phantmoney2011@gmail.com>
2022-09-15 22:37:09 +0100

    Lists all the tables of my database in my MySQL server

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,6 @@
 -- Lists all the tables of my database in my MySQL server
+-- Write a script that lists all the tables of a database in your MySQL server.
+
+-- The database name will be passed as argument of mysql command (in the following example: mysql is the name of the database)
+
 SHOW TABLES;
