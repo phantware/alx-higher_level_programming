1dcca2747447a5a02a0998650b38298726c46a17
phantware <phantmoney2011@gmail.com>
2022-09-15 01:07:54 +0100

    How to use map function understood

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
-console.log(list);
-console.log(list.map((item, index) => item * index));
+const list = require('./test/100-data.js').list
+console.log(list)
+console.log(list.map((item, index) => item * index))
