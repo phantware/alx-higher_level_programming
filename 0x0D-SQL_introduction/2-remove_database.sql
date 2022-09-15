0f31df8a4f4abdd741b1ea0011d0095ce190462a
phantware <phantmoney2011@gmail.com>
2022-09-15 22:36:02 +0100

    deletes the database hbtn_0c_0 in my MySQL server

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,7 @@
 -- deletes the database hbtn_0c_0 in my MySQL server
+-- Write a script that deletes the database hbtn_0c_0 in your MySQL server.
+
+-- If the database hbtn_0c_0 doesn’t exist, your script should not fail
+-- You are not allowed to use the SELECT or SHOW statements
+
 DROP DATABASE IF EXISTS `hbtn_0c_0`;
