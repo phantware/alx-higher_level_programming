73c23f0bcb1dd892010b6f8c24c1dec8cc4c62fa
phantware <phantmoney2011@gmail.com>
2022-09-16 16:22:55 +0100

    lists all shows from hbtn_0d_tvshows_rate by their rating

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,15 @@
 -- lists all shows from hbtn_0d_tvshows_rate by their rating
 -- lists all rows of a table by the sum of a linked row
+
+-- Import the database hbtn_0d_tvshows_rate dump to your MySQL server: download
+
+-- Write a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+
+-- Each record should display: tv_shows.title - rating sum
+-- Results must be sorted in descending order by the rating
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+
 SELECT title, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_shows
 LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
