f740ff4f51aa7211594b71abf788b0926972df15
phantware <phantmoney2011@gmail.com>
2022-09-16 16:06:21 +0100

    lists all Comedy shows in the database hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,16 @@
 -- lists all Comedy shows in the database hbtn_0d_tvshows
 -- lists all rows of a database corresponding to a column value
+
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 14-my_genres.sql)
+
+-- Write a script that lists all Comedy shows in the database hbtn_0d_tvshows.
+
+-- The tv_genres table contains only one record where name = Comedy (but the id can be different)
+-- Each record should display: tv_shows.title
+-- Results must be sorted in ascending order by the show title
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+
 SELECT title
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
