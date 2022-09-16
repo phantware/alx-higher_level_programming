c1e5459321492aaba13b4fb0006699ec1f4f00dd
phantware <phantmoney2011@gmail.com>
2022-09-16 12:56:03 +0100

    creates the table force_name on your MySQL server

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,12 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2
+
+-- Write a script that creates the database hbtn_0d_2 and the user user_0d_2.
+
+-- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+-- The user_0d_2 password should be set to user_0d_2_pwd
+-- If the database hbtn_0d_2 already exists, your script should not fail
+-- If the user user_0d_2 already exists, your script should not fail
+
 -- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- creates a user
--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,12 @@
 -- creates the table force_name on your MySQL server
+
+-- Write a script that creates the table force_name on your MySQL server.
+
+-- force_name description:
+-- id INT
+-- name VARCHAR(256) can’t be null
+-- The database name will be passed as an argument of the mysql command
+-- If the table force_name already exists, your script should not fail
+
 -- creates a table in a database
 CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
