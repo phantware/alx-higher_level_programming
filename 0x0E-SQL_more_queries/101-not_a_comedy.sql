1c24cf48092c5da5b916cff1e54f9e3fdbfb0432
phantware <phantmoney2011@gmail.com>
2022-09-16 16:21:32 +0100

    lists all shows without the genre Comedy in the database hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,5 +1,16 @@
 -- lists all shows without the genre Comedy in the database hbtn_0d_tvshows
 -- uses a database to list all rows not linked to one row
+
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 100-not_my_genres.sql)
+
+-- Write a script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+
+-- The tv_genres table contains only one record where name = Comedy (but the id can be different)
+-- Each record should display: tv_shows.title
+-- Results must be sorted in ascending order by the show title
+-- You can use a maximum of two SELECT statement
+-- The database name will be passed as an argument of the mysql command
+
 SELECT title
 FROM tv_shows
 WHERE title NOT IN
