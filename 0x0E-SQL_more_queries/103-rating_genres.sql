11c9bc9f14fc044456ce5fc81047e4cb95b57cb0
phantware <phantmoney2011@gmail.com>
2022-09-16 16:25:56 +0100

    lists all genres in the database hbtn_0d_tvshows_rate by their rating

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,5 +1,15 @@
 -- lists all genres in the database hbtn_0d_tvshows_rate by their rating
 -- lists all rows in a database linked to a row in another table
+
+-- Import the database dump from hbtn_0d_tvshows_rate to your MySQL server: download (same as 102-rating_shows.sql)
+
+-- Write a script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+
+-- Each record should display: tv_genres.name - rating sum
+-- Results must be sorted in descending order by their rating
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+
 SELECT name, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
