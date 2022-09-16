5c2e9f3091a2c941a594b74ef4de64109b2dfb91
phantware <phantmoney2011@gmail.com>
2022-09-16 15:58:35 +0100

    lists all shows contained in hbtn_0d_tvshows without a genre linked

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,7 @@
 -- lists all shows contained in the database hbtn_0d_tvshows
 -- lists all rows of tables in a database
 
-Import the database dump of hbtn_0d_tvshows to your MySQL server: download (same as 10-genre_id_by_show.sql)
+-- Import the database dump of hbtn_0d_tvshows to your MySQL server: download (same as 10-genre_id_by_show.sql)
 
 -- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
 
