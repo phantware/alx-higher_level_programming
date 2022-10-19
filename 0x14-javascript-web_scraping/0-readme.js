8d49f463ecbca7f6b1378cd00e9e166eed0789e9
phantware <phantmoney2011@gmail.com>
2022-10-19 21:14:40 +0100

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
 
-// Write a script that reads and prints the content of a file.
-
-// The first argument is the file path
-// The content of the file must be read in utf-8
-// If an error occurred during the reading, print the error object
-
 const fs = require('fs')
 const FileName = process.argv[2]
 fs.readFile(FileName, 'utf8', function (err, line) {
